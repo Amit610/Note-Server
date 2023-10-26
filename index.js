b5204f51b8e0836215f5f9dbe7929972d7fcdc92
Amit <amitjadhav610@gmail.com>
2023-10-27 00:15:56 +0530

    Updated

--- index.js
+++ index.js
@@ -6,14 +6,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
-const corsOptions = {
-  origin: 'http://localhost:3001', // Replace with your frontend's URL
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // If you're using cookies or authentication
-  optionsSuccessStatus: 204, // Some legacy browsers (IE) choke on 204
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 // Available routes
